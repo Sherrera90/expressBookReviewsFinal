147229d481fb61b5e82b9a73caa33eb6f302396d
Sherrera90 <Steph.herrera90@gmail.com>
2024-03-08 13:35:14 -0500

    authentication index

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,12 +12,11 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-        res.send(JSON.stringify(books,null,4));
-      return res.status(300).json({message: "Yet to be implemented"});
-    });
+  //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
+});
 
-// Get book details based on ISBN working?
+// Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
